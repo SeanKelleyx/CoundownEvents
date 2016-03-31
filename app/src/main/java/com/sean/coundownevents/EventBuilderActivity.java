0bba50d63ce2959c8bd65a4703f422e93b62ae5e
Sean Kelley <s.kelley27@gmail.com>
2016-03-31 18:06:16 -0500

    add title for Event builder activity

--- app/src/main/java/com/sean/coundownevents/EventBuilderActivity.java
+++ app/src/main/java/com/sean/coundownevents/EventBuilderActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class EventBuilder extends AppCompatActivity {
+public class EventBuilderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +16,7 @@ public class EventBuilder extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
