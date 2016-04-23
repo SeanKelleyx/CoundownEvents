e9746828f2d523711f3aceaa8c4ca4f434f18bfa
Sean Kelley <s.kelley27@gmail.com>
2016-04-22 20:18:47 -0500

    change datetime to integer and backgroung image to blob

--- app/src/main/java/com/sean/coundownevents/database/CountdownEventReaderDbHelper.java
+++ app/src/main/java/com/sean/coundownevents/database/CountdownEventReaderDbHelper.java
@@ -12,13 +12,15 @@ public class CountdownEventReaderDbHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "Countdown.db";
     private static final String TEXT_TYPE = " TEXT";
+    private static final String INTEGER_TYPE = " INTEGER";
+    private static final String BLOB_TYPE = " BLOB";
     private static final String COMMA_SEP = ",";
     private static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + CountdownEventReaderContract.CountdownEventEntry.TABLE_NAME + " (" +
                     CountdownEventReaderContract.CountdownEventEntry._ID + " INTEGER PRIMARY KEY," +
                     CountdownEventReaderContract.CountdownEventEntry.COLUMN_NAME_EVENT_TITLE + TEXT_TYPE + COMMA_SEP +
-                    CountdownEventReaderContract.CountdownEventEntry.COLUMN_NAME_EVENT_DATETIME + TEXT_TYPE + COMMA_SEP +
-                    CountdownEventReaderContract.CountdownEventEntry.COLUMN_NAME_BACKGROUND_COLOR + TEXT_TYPE +
+                    CountdownEventReaderContract.CountdownEventEntry.COLUMN_NAME_EVENT_DATETIME + INTEGER_TYPE + COMMA_SEP +
+                    CountdownEventReaderContract.CountdownEventEntry.COLUMN_NAME_BACKGROUND_IMAGE + BLOB_TYPE +
                     " )";
 
     private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + CountdownEventReaderContract.CountdownEventEntry.TABLE_NAME;
